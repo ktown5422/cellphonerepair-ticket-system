6a1ce433d9b789c4a82349d7331496df1a8ca33f
ktown5422 <ktown5422@gmail.com>
2020-10-04 13:49:05 -0500

    fix mysql localhost connection issue

--- app.js
+++ app.js
@@ -1,27 +1,105 @@
-const express = require("express");
-const mysql = require("mysql");
+const express = require('express');
+const mysql = require('mysql');
+
+// Create connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 8889,
+    user: 'ticket-system',
+    password: 'ticket-system',
+    database: 'ticket-system'
+});
+
+// Connect
+db.connect((err) => {
+    if(err){
+      console.log(err)
+    } else {
+      console.log('MySql is Connected')
+    }
+})
 
 const app = express();
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'nodejs-login-db'
+
+// Create table
+app.get('/createpoststable', (req, res) => {
+    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY(id))';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Posts table created...');
+    });
 });
 
-db.connect( (error) => {
-    if(error) {
-        console.log(error)
-    } else {
-        console.log("MYSQL is Connected")
-    }
-} )
+// Insert post 1
+app.get('/addpost1', (req, res) => {
+    let post = {title:'Post One', body:'This is post number one'};
+    let sql = 'INSERT INTO posts SET ?';
+    let query = db.query(sql, post, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Post 1 added...');
+    });
+});
 
-app.get("/", (req, res) => {
-    res.send("<h1>Home Page</h1>")
-}); 
+function createTable(){
 
-app.listen(5002, () => {
-    console.log("Server started on Port 5002");
-})
+}
+
+// Insert post 2
+app.get('/addpost2', (req, res) => {
+    let post = {title:'Post Two', body:'This is post number two'};
+    let sql = 'INSERT INTO posts SET ?';
+    let query = db.query(sql, post, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Post 2 added...');
+    });
+});
+
+// Select posts
+app.get('/getposts', (req, res) => {
+    let sql = 'SELECT * FROM posts';
+    let query = db.query(sql, (err, results) => {
+        if(err) throw err;
+        console.log(results);
+        res.send('Posts fetched...');
+    });
+});
+
+// Select single post
+app.get('/getpost/:id', (req, res) => {
+    let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Post fetched...');
+    });
+});
+
+// Update post
+app.get('/updatepost/:id', (req, res) => {
+    let newTitle = 'Updated Title';
+    let sql = `UPDATE posts SET title = '${newTitle}' WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Post updated...');
+    });
+});
+
+// Delete post
+app.get('/deletepost/:id', (req, res) => {
+    let newTitle = 'Updated Title';
+    let sql = `DELETE FROM posts WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Post deleted...');
+    });
+});
+
+app.listen('3000', () => {
+    console.log('Server started on port 3000');
+});

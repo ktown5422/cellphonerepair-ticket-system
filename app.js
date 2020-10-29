a88e127628946342b067981f85a7429352f1c26a
ktown5422 <ktown5422@gmail.com>
2020-10-29 13:07:22 -0500

    update delete route

--- app.js
+++ app.js
@@ -199,7 +199,7 @@ app.get('/delete/:id', (req, res) => {
     let query = db.query(sql, (err, result) => {
         if(err) throw err;
         console.log(result);
-        res.send('ticket deleted...');
+        res.redirect('/dashboard');
     });
 });
 

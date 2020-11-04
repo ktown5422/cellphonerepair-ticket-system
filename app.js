350af164188e46210be424b89a5620fa04e21494
ktown5422 <ktown5422@gmail.com>
2020-11-03 20:38:12 -0600

    update to database

--- app.js
+++ app.js
@@ -288,6 +288,6 @@ app.get('/delete/:id', (req, res) => {
 //     });
 // });
 
-app.listen('3000', () => {
-    console.log('Server started on port 3000');
-});
+// app.listen('3000', () => {
+//     console.log('Server started on port 3000');
+// });
